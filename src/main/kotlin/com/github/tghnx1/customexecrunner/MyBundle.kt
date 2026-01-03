311fa29dee417ab11fe92dfa46274c0a1b56119c
GitHub Action <action@github.com>
2026-01-03 15:26:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/tghnx1/customexecrunner/MyBundle.kt
+++ src/main/kotlin/com/github/tghnx1/customexecrunner/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.tghnx1.customexecrunner
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/tghnx1/customexecrunner/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/tghnx1/customexecrunner/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.tghnx1.customexecrunner.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
